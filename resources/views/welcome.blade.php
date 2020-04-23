8c2e333482e6e3cb5540710da4501dda69846fba
KyawZayya <kyawzayya.ucsp@gmail.com>
2020-04-23 17:28:56 +0630

    Added Welcome Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,93 +8,32 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+        <div class="penguin">
+            <div class="penguin-bottom">
+              <div class="right-hand"></div>
+              <div class="left-hand"></div>
+              <div class="right-feet"></div>
+              <div class="left-feet"></div>
             </div>
-        </div>
+            <div class="penguin-top">
+              <div class="right-cheek"></div>
+              <div class="left-cheek"></div>
+              <div class="belly"></div>
+              <div class="right-eye">
+                <div class="sparkle"></div>
+              </div>
+              <div class="left-eye">
+                <div class="sparkle"></div>
+              </div>
+              <div class="blush-right"></div>
+              <div class="blush-left"></div>
+              <div class="beak-top"></div>
+              <div class="beak-bottom"></div>
+            </div>
+          </div>
     </body>
 </html>
